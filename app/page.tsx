e69cabf0287ba74511841e53a802ad8a5eccf406
anthonyhfranca <aharisson@uolinc.com>
2024-08-01 18:04:02 -0300

    feat: criando bloco como usar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/Partials/Home/Banner";
 import About from "@/Partials/Home/About";
+import HowToUse from "@/Partials/Home/HowToUse";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
    <>
      <Banner />
      <About />
+     <HowToUse />
    </>
   );
 }
