a96b3ca340a9ad3d5b91b5dd41c1be6f33d44ebb
anthonyhfranca <aharisson@uolinc.com>
2024-08-15 18:56:30 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Banner from "@/components/banner";
+import Banner from "@/Partials/Home/Banner";
 import Contact from "@/components/Contact";
 import About from "@/Partials/Home/About";
 import HowToUse from "@/Partials/Home/HowToUse";
